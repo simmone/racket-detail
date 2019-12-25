6f4b549e8ff3dd81c7ec7c17f149d62d969f6721
Chen Xiao <chenxiao770117@gmail.com>
2019-12-25 14:48:19 +0800

    update

--- detail/src/report/pdf.rkt
+++ detail/src/report/pdf.rkt
@@ -28,38 +28,40 @@
                   (let* ([page (car loop_pages)]
                          [prefix_length (DETAIL-PAGE-prefix_length page)])
 
-                    (send dc start-page)
-
-                    (let loop-rec ([recs (DETAIL-PAGE-recs page)]
-                                   [loop_line 0])
-                      (when (not (null? recs))
-                            (let* ([rec (car recs)]
-                                   [type (DETAIL-REC-type rec)]
-                                   [prefix (DETAIL-REC-prefix rec)]
-                                   [data (DETAIL-REC-data rec)])
-                              (cond
-                               [(eq? type 'h1)
-                                (send dc set-font (make-font #:size 36))
-                                (send dc draw-text data 0 loop_line)
-                                (send dc set-font (make-font #:size 14))
-                                (loop-rec (cdr recs) (+ loop_line 80))]
-                               [(eq? type 'h2)
-                                (send dc set-font (make-font #:size 24))
-                                (send dc draw-text data 0 loop_line)
-                                (send dc set-font (make-font #:size 14))
-                                (loop-rec (cdr recs) (+ loop_line 60))]
-                               [(eq? type 'h3)
-                                (send dc set-font (make-font #:size 20))
-                                (send dc draw-text data 0 loop_line)
-                                (send dc set-font (make-font #:size 14))
-                                (loop-rec (cdr recs) (+ loop_line 50))]
-                               [(eq? type 'line)
-                                (if (string=? data "")
-                                    (send dc draw-text data 0 "\n")
-                                    (send dc draw-text data 0
-                                          (format "~a: ~a\n" (~a #:min-width prefix_length #:pad-string " " #:align 'right prefix) data)))
-                                (loop-rec (cdr recs) (+ loop_line 32))]))))
-                    (send dc end-page)
-                    (loop-page (cdr loop_pages))))))
+                    (dynamic-wind
+                        (lambda () (send dc start-page))
+                        (lambda ()
+                          (let loop-rec ([recs (DETAIL-PAGE-recs page)]
+                                         [loop_line 0])
+                            (when (not (null? recs))
+                                  (let* ([rec (car recs)]
+                                         [type (DETAIL-REC-type rec)]
+                                         [prefix (DETAIL-REC-prefix rec)]
+                                         [data (DETAIL-REC-data rec)])
+                                    (cond
+                                     [(eq? type 'h1)
+                                      (send dc set-font (make-font #:size 36))
+                                      (send dc draw-text data 0 loop_line)
+                                      (send dc set-font (make-font #:size 14))
+                                      (loop-rec (cdr recs) (+ loop_line 80))]
+                                     [(eq? type 'h2)
+                                      (send dc set-font (make-font #:size 24))
+                                      (send dc draw-text data 0 loop_line)
+                                      (send dc set-font (make-font #:size 14))
+                                      (loop-rec (cdr recs) (+ loop_line 60))]
+                                     [(eq? type 'h3)
+                                      (send dc set-font (make-font #:size 20))
+                                      (send dc draw-text data 0 loop_line)
+                                      (send dc set-font (make-font #:size 14))
+                                      (loop-rec (cdr recs) (+ loop_line 50))]
+                                     [(eq? type 'line)
+                                      (if (string=? data "")
+                                          (send dc draw-text "\n" 0 loop_line)
+                                          (begin
+                                            (send dc draw-text (format "~a\n" (~a #:min-width prefix_length #:pad-string " " #:align 'right prefix)) 0 loop_line)
+                                            (send dc draw-text (format ": ~a" data) (* prefix_length 10) (+ loop_line 3))))
+                                      (loop-rec (cdr recs) (+ loop_line 32))])))))
+                        (lambda () (send dc end-page))))
+                    (loop-page (cdr loop_pages)))))
         (lambda ()
           (send dc end-doc)))))
