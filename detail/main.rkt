cad536473b790151b2abde580643b768bcf181da
Chen Xiao <chenxiao770117@gmail.com>
2019-12-27 16:09:24 +0800

    update

--- detail/main.rkt
+++ detail/main.rkt
@@ -5,10 +5,11 @@
 
 (provide (contract-out
           [detail (-> (or/c #f (listof (or/c 'raw 'console path-string?))) procedure? any)]
-          [detail-add-rec (-> DETAIL-REC? void?)]
+          [detail-add-rec (-> (or/c DETAIL-TITLE? DETAIL-LINE? DETAIL-PREFIX-LINE?) void?)]
           [detail-h1 (-> string? void?)]
           [detail-h2 (-> string? void?)]
           [detail-h3 (-> string? void?)]
-          [detail-line (-> string? string? void?)]
+          [detail-line (-> string? void?)]
+          [detail-prefix-line (-> string? string? void?)]
           [detail-page (-> procedure? void?)]
           ))
