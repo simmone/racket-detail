8a6f3f1fb3ebeb5587758a95e9775134f6c0b705
Chen Xiao <chenxiao770117@gmail.com>
2020-01-11 06:44:32 +0800

    update

--- detail/effect/test-list.rkt
+++ detail/effect/test-list.rkt
@@ -4,7 +4,7 @@
 
 (define (test-line-break)
   (detail
-   #:formats '(raw console "line_break.pdf")
+   #:formats '(raw console "list.pdf")
    #:line_break_length 1
    #:font_size 'small
    (lambda ()
@@ -31,6 +31,29 @@
            (detail-row
             (lambda ()
               (detail-col "12345")
-              (detail-col "123"))))))))))
+              (detail-col "123")))))
+
+        (detail-h1 "Hello World!")
+
+        (detail-line "1234567890")
+        
+        (detail-line "Yes, one by one, day by day.")
+
+        (detail-list
+         (lambda ()
+           (detail-row
+            (lambda ()
+              (detail-col "123")
+              (detail-col "12345")
+              (detail-col "12345678901" #:width 3)
+              (detail-col "123456789012345678901" #:width 10)
+              ))
+
+           (detail-row
+            (lambda ()
+              (detail-col "12345")
+              (detail-col "123")))))
+        
+        )))))
 
 (test-line-break)
---
+++ detail/effect/test-page-split.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(require "../main.rkt")
+
+(define (test-line-break)
+  (detail
+   #:formats '(raw console "page.pdf")
+   (lambda ()
+     (detail-page
+      (lambda ()
+        (detail-list
+         (lambda ()
+           (detail-row (lambda () (detail-col "123456789012345678901" #:width 1)))
+           (detail-row (lambda () (detail-col "123456789012345678901" #:width 1)))
+           )))))))
+
+(test-line-break)
