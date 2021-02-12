19001e6002d7921980b51d31bfc1da1286e2417d
Chen Xiao <chenxiao770117@gmail.com>
2021-02-12 08:40:45 +0800

    update

--- detail/info.rkt
+++ detail/info.rkt
@@ -3,6 +3,6 @@
   '(("scribble/detail.scrbl" (multi-page) (tool 100))))
 
 (define compile-omit-paths '("test" "effect"))
-(define test-includes-paths '("test"))
+(define test-omit-paths '("src" "main.rkt" "scribble" "info.rkt" "example" "effect"))
 
 
